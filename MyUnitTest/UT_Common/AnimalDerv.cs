b54cec8175e2d69c91a06c477f2d21e524039351
hawk_wei <hawk_wei@htc.com>
2021-04-20 14:08:28 +0800

    Class For Test

---
+++ MyUnitTest/UT_Common/AnimalDerv.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp.Utils;
+
+namespace MyUnitTest.UT_Common
+{
+    class AnimalDerv
+    {
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+
+    public class Human : Animal
+    {
+        public string Name { get; set; }
+
+        public Human(string name) { Name = name; }
+        public void Say() { HLog.print("Hello everyone!"); }
+    }
+
+    public class Dog
+    {
+        public string Name { get; set; }
+
+        public Dog(string breedName) { Name = breedName; }
+        public void Bark() { HLog.print("WonWoonn~~~~~~"); }
+    }
+}
---
+++ MyUnitTest/UT_Common/HDebugger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyUnitTest.UT_Common
+{
+    class HDebugger
+    {
+        static public void waitForDebugger() {
+            Console.WriteLine("Waiting for debugger to attach");
+            while (!System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.WriteLine("Debugger attached");
+        }
+    }
+}
