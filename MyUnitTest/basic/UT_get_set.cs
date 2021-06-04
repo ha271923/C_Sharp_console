a085ee50c6c064c1973332f1126839ac430cddc2
hawk_wei <hawk_wei@htc.com>
2021-06-04 16:29:13 +0800

    get set keyword

---
+++ MyUnitTest/basic/UT_get_set.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using ConsoleApp.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitTest.basic
+{
+    class UT_get_set
+    {
+        public int a;
+        public int b { get; }  // read-only
+        public int c { get; set; }
+
+        [TestMethod] // -----------------------------------------------------------------------
+        public void TestMethod1()
+        {
+            a = 1;
+            // b = 2;   
+            c = 3;
+
+        }
+    }
+}
